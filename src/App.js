11d96f62ff9577a55662a53409417ab331f955c2
Tristan <tristanchay123@gmail.com>
2023-08-29 13:49:35 +0800

    added links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import './App.css';
+import "./App.css";
 import TypeWriterEffect from "react-typewriter-effect";
+import LinkedinLogo from "./icons/linkedin.png";
+import GithubLogo from "./icons/github.png";
+import InstagramLogo from "./icons/instagram.png";
 
 function App() {
+  const InstagramLink = (props) => (
+    <a href="https://instagram.com/cheesetan_" style={{ margin: 5 }}>
+      <img src={InstagramLogo} alt="Instagram" className="profileURL" />
+    </a>
+  );
+
+  const LinkedinLink = (props) => (
+    <a href="https://linkedin.com/in/tristanchay" style={{ margin: 5 }}>
+      <img src={LinkedinLogo} alt="Linkedin" className="profileURL" />
+    </a>
+  );
+
+  const GithubLink = (props) => (
+    <a href="https://github.com/cheesetan" style={{ margin: 5 }}>
+      <img src={GithubLogo} alt="Github" className="profileURL" />
+    </a>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{margin: 0}}>Hello! I'm Tristan Chay!</h1>
-        <TypeWriterEffect
-                  textStyle={{
-                    fontSize: "32px",
-                    color: "#9BA4B5",
-                  }}
-                  cursorColor="#E1E3FF"
-                  text={"i'm a mobile developer :)"}
-                  typeSpeed={50}
-                />     
+        <section id="introSection">
+          <div style={{ margin: 15 }}>
+            <h1 style={{ margin: 0 }}>Hello! I'm Tristan Chay!</h1>
+            <TypeWriterEffect
+              textStyle={{
+                textAlign: "center",
+                fontSize: "32px",
+                color: "#9BA4B5",
+              }}
+              cursorColor="#FFFFFF"
+              text={"i'm a mobile developer :)"}
+              typeSpeed={35}
+            />
+
+            <div style={{ marginTop: 15 }}>
+              <InstagramLink />
+              <LinkedinLink />
+              <GithubLink />
+            </div>
+          </div>
+
+          <div></div>
+        </section>
       </header>
     </div>
   );
