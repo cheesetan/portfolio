ada3c20b5a61f9e83e261ffd466c53d12be939c3
Tristan Chay <tristanchay123@gmail.com>
2024-06-17 18:07:43 +0800

    Initial commit

---
+++ src/data/user.js
@@ -0,0 +1,156 @@
+const INFO = {
+	main: {
+		title: "Tristan Chay",
+		name: "Tristan Chay",
+		email: "tristanchay123@gmail.com",
+		logo: "",
+	},
+
+	socials: {
+		github: "https://github.com/cheesetan/",
+		linkedin: "https://linkedin.com/in/tristanchay/",
+	},
+
+	homepage: {
+		title: "Hey, I'm Tristan Chay!",
+		subtitle: "Student Developer",
+		description:
+			"I'm a 15 year old developer from Singapore. I help to mentor juniors at Swift Accelerator Programme, guiding them in their iOS App Development journey. Additionally, I am also one of 350 winners worldwide in Apple's WWDC23 Swift Student Challenge, and I am Apple App Development with Swift Associate certified too.",
+	},
+
+	about: {
+		title: "I'm Tristan Chay. I live in Singapore, where I help to develop the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "Featured Articles",
+		description:
+			"Here are some of the articles that I have been featured in.",
+	},
+
+	speicalProjects: [
+		{
+			title: "ExiSST",
+			description:
+				"ExiSST is a secure, private, and verifiable way of taking attendance through the use of a refreshing QR Code and Google Sheets integration.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View Project",
+			link: "https://github.com/attendinc-group/exisst",
+		},
+
+		{
+			title: "Glassroom",
+			description:
+				"Glassroom is a native Google Classroom client that provides features such as speed improvements, instantaneous loading with cache, and user convenience enhancements.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/glassroom/id6476105446",
+		},
+
+		{
+			title: "GrowCalth",
+			description:
+				"GrowCalth is a one stop platform that allows SST Students to participate in house challenges and further fosters house spirit among their house members.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/growcalth/id6456388202",
+		},
+
+		{
+			title: "MathX.",
+			description:
+				"MathX features a useful tools such as calculators with special functions to help you quickly and easily calculate and solve your mathematical needs.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/mathx/id1434950136",
+		},
+
+		{
+			title: "Pawtfolio",
+			description:
+				"Pawtfolio aims to be able to guide students on their journey of creating and making their portfolios.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/pawtfolio/id6473139075",
+		},
+
+		{
+			title: "SST Open House 2024",
+			description:
+				"The SST Open House 2024 app allows visitors to explore the campus with ease, access detailed event information, and even compete for exciting prizes right within the app.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/sst-open-house-2024/id6479939416",
+		},
+	],
+
+	projects: [
+
+		{
+			title: "ExiSST",
+			description:
+				"ExiSST is a secure, private, and verifiable way of taking attendance through the use of a refreshing QR Code and Google Sheets integration.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View Project",
+			link: "https://github.com/attendinc-group/exisst",
+		},
+
+		{
+			title: "Glassroom",
+			description:
+				"Glassroom is a native Google Classroom client that provides features such as speed improvements, instantaneous loading with cache, and user convenience enhancements.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/glassroom/id6476105446",
+		},
+
+		{
+			title: "GrowCalth",
+			description:
+				"GrowCalth is a one stop platform that allows SST Students to participate in house challenges and further fosters house spirit among their house members.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/growcalth/id6456388202",
+		},
+
+		{
+			title: "MathX.",
+			description:
+				"MathX features a useful tools such as calculators with special functions to help you quickly and easily calculate and solve your mathematical needs.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/mathx/id1434950136",
+		},
+
+		{
+			title: "Pawtfolio",
+			description:
+				"Pawtfolio aims to be able to guide students on their journey of creating and making their portfolios.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/pawtfolio/id6473139075",
+		},
+
+		{
+			title: "SST Open House 2024",
+			description:
+				"The SST Open House 2024 app allows visitors to explore the campus with ease, access detailed event information, and even compete for exciting prizes right within the app.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/swift/swift.png",
+			linkText: "View on App Store",
+			link: "https://apps.apple.com/us/app/sst-open-house-2024/id6479939416",
+		},
+
+		{
+			title: "Virtual Photobooth",
+			description:
+				"Virtual Photobooth is a Python program that makes use of cv2 to overlay props on top of a live video feed, allowing users to take images with virtual props.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/cheesetan/virtual-photobooth",
+		},
+	],
+};
+
+export default INFO;
